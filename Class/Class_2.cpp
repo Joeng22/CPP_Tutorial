fad63fc8f40dcfacc51d610d45cfedcefee68350
Joe <joeng22@gmail.com>
2024-07-08 00:03:37 +0530

    Cpp class sample interview questions

--- Class/Class_2.cpp
+++ Class/Class_2.cpp
@@ -7,6 +7,15 @@ Class Inhertence
 Notes :
 private - members cannot be accessed (or viewed) from outside the class. 
 protected - members cannot be accessed from outside the class, however, they can be accessed in inherited classes.
+
+
+Public, Protected, and Private inheritance in C++
+
+public inheritance makes public members of the base class public in the derived class, and the protected members of the base class remain protected in the derived class.
+protected inheritance makes the public and protected members of the base class protected in the derived class.
+private inheritance makes the public and protected members of the base class private in the derived class.
+
+
 */
 
 #include<iostream>
@@ -16,7 +25,7 @@ using namespace std;
 
 class Human
 {
-    public:
+    protected:
         int Age;
         string Gender;
         string Name;
